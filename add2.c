dc0265fe5347b6d170068bbce4e064759c03776f
Ohmsai <ohmsainirmalkar@gmail.com>
2025-08-21 05:06:28 +0000

    Add of two numbers.

---
+++ add2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int num1 , num2 , sum ;
+printf("Enter the first number to add: \n");
+scanf("%d", &num1);
+printf("Enter the second number to add: \n");
+scanf("%d", &num2);
+sum = num1+num2;
+printf("Sum of the numbers is: %d.\n" ,sum);
+return 0;
+}
+
